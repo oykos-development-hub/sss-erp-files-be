db586bbc91b779102d4872913b8ba01b6be4e3ed
oykos-development-hub <hub@oykos.me>
2023-11-03 13:13:50 +0000

    Update code from GitLab

--- routes.go
+++ routes.go
@@ -5,23 +5,34 @@ import (
 	"gitlab.sudovi.me/erp/file-ms-api/middleware"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/oykos-development-hub/celeritas"
 )
 
 func routes(app *celeritas.Celeritas, middleware *middleware.Middleware, handlers *handlers.Handlers) *chi.Mux {
 	// middleware must come before any routes
 
-	//api
-	app.Routes.Route("/api", func(rt chi.Router) {
+	// Kreirajte novi router
+	r := chi.NewRouter()
 
+	// Konfigurišite CORS
+	cors := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	})
+
+	r.Use(cors.Handler)
+
+	r.Route("/api", func(rt chi.Router) {
 		rt.Post("/files", handlers.FileHandler.CreateFile)
 		rt.Get("/files/{id}", handlers.FileHandler.GetFileById)
 		rt.Delete("/files/{id}", handlers.FileHandler.DeleteFile)
-
 		rt.Get("/download/*", handlers.FileHandler.GetFile)
 		rt.Get("/file-overview/{id}", handlers.FileHandler.FileOverview)
-
 	})
 
-	return app.Routes
+	return r
 }
